84de2a03dc618328745de2c385bb5ca0a1c4eb14
johnvente <jhon.vente@edunext.co>
2023-11-15 21:35:59 -0500

    fix: removing delete action for files and adding delete folders only

--- react-app/components/FileManager/constants.js
+++ react-app/components/FileManager/constants.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import { ChonkyActions, ChonkyIconName, defineFileAction } from 'chonky';
 
 export const prepareCustomFileMap = () => {
   const rootFolderId = uuidv4();
@@ -15,3 +16,23 @@ export const prepareCustomFileMap = () => {
 
   return { baseFileMap, rootFolderId };
 };
+
+// Custom action
+export const deleteFolderAction = defineFileAction({
+  id: 'delete_folder',
+  button: {
+    name: 'Delete folders',
+    toolbar: false,
+    contextMenu: true,
+    icon: ChonkyIconName.trash
+  }
+});
+
+export const defaultFileActions = [ChonkyActions.CreateFolder, ChonkyActions.UploadFiles, ChonkyActions.DownloadFiles];
+
+export const customFileActions = [
+  ChonkyActions.CreateFolder,
+  ChonkyActions.UploadFiles,
+  deleteFolderAction,
+  ChonkyActions.DownloadFiles
+];
