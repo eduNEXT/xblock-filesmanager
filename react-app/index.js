1a755426c011311896ee6a5c30a5f2f192551154
johnvente <jhon.vente@edunext.co>
2023-12-27 15:38:29 -0500

    feat: event track for downloaded files

--- react-app/index.js
+++ react-app/index.js
@@ -25,9 +25,11 @@ if (process.env.NODE_ENV === 'development') {
 function FilesManagerXBlock(runtime, _, context) {
   const xblockId = context.xblock_id;
   const elementSelector = document.querySelector(`[data-usage-id="${xblockId}"]`);
+  const typeRuntime = elementSelector.getAttribute('data-runtime-class');
   xBlockContext.runtime = runtime;
   xBlockContext.element = elementSelector;
   xBlockContext.context = context;
+  xBlockContext.isStudioView = elementSelector && typeRuntime !== 'LmsRuntime';
   xBlockContext.isEditView = context.is_edit_view;
   const rootElement = document.getElementById('files-manager-app-root');
   const root = ReactDOM.createRoot(rootElement);
