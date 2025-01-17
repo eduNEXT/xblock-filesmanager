fbd8ee2cece7f826ce2ea113e6e5bee8d611d765
Luis Felipe Castano <felipe.castano@edunext.co>
2025-01-17 09:08:49 -0500

    chore: support django 3.2

--- setup.py
+++ setup.py
@@ -159,6 +159,8 @@ setup(
     keywords='Python edx',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
